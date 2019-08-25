fb410ef0c558b039071ed8edded760f475a72f02
mumusmssb <956045916@qq.com>
2019-08-25 15:27:49 +0800

    添加sql

--- src/main/java/com/like/community/controller/AuthorizeController.java
+++ src/main/java/com/like/community/controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package com.like.community.controller;
 
 import com.like.community.dto.AccessToKenDTO;
 import com.like.community.dto.GitHubUser;
+import com.like.community.mapper.UserMapper;
+import com.like.community.model.User;
 import com.like.community.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -22,9 +27,13 @@ public class AuthorizeController {
     @Value("${GitHub.redirect.uri}")
     private String redirectURI;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessToKenDTO accessToKenDTO = new AccessToKenDTO();
         accessToKenDTO.setClient_id(clientID);
         accessToKenDTO.setClient_secret(clientSecret);
@@ -32,10 +41,22 @@ public class AuthorizeController {
         accessToKenDTO.setCode(code);
         accessToKenDTO.setState(state);
         String accessToKen = gitHubProvider.getAccessToKen(accessToKenDTO);
-        GitHubUser user = gitHubProvider.getGitHubUser(accessToKen);
-        System.out.println(user.getName());
-        System.out.println(user.getBio());
-        System.out.println(user.getId());
-        return "index";
+        GitHubUser githubUser = gitHubProvider.getGitHubUser(accessToKen);
+        if (githubUser != null){
+            //登录成功  写入cookie 和 session
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else{
+            //登录失败
+            return "redirect:/";
+        }
+
     }
 }
